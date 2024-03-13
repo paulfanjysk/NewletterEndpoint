903cfab982ec5ace9d5426463add4cb39f89ac2e
paulfanjysk <73196979+paulfanjysk@users.noreply.github.com>
2024-03-13 15:19:39 -0700

    Update Subscription.php

--- Model/Subscription.php
+++ Model/Subscription.php
@@ -1,17 +1,20 @@
 <?php
 namespace JYSK\NewsletterEndpoint\Model;
+
 use JYSK\NewsletterEndpoint\Api\SubscriptionInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 
 class Subscription implements SubscriptionInterface
 {
     protected $subscriberFactory;
+    protected $customer;
+    protected $storemanager;
 
     public function __construct(
         \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory,
         \Magento\Customer\Model\CustomerFactory $customer,
-        \Magento\Store\Model\StoreManagerInterface $storemanager)
-    {
+        \Magento\Store\Model\StoreManagerInterface $storemanager
+    ) {
         $this->subscriberFactory = $subscriberFactory;
         $this->customer = $customer;
         $this->storemanager = $storemanager;
@@ -52,10 +55,20 @@ class Subscription implements SubscriptionInterface
     {
         $websiteId = $this->storemanager->getStore()->getWebsiteId();
         $customerId = $this->customer->create()->setWebsiteId($websiteId)->loadByEmail($email)->getId();
+        
+        // Check if the email belongs to a customer
         if ($customerId) {
             $this->subscriberFactory->create()->unsubscribeCustomerById($customerId);
-        } else {
-            $this->subscriberFactory->create()->loadByEmail($email)->unsubscribe();
+            return; // Unsubscribed successfully
         }
+        
+        // Check if the email exists in the newsletter subscriber table
+        $subscriber = $this->subscriberFactory->create()->loadByEmail($email);
+        if ($subscriber->getId()) {
+            $subscriber->unsubscribe();
+            return; // Unsubscribed successfully
+        }
+        
+        // If email is neither a customer nor a newsletter subscriber, do nothing
     }
 }
