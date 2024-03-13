5c215bbe5cdbd265926bff7e0acf4c6eb4a472c6
paulfanjysk <73196979+paulfanjysk@users.noreply.github.com>
2024-03-13 15:18:51 -0700

    Add files via upload

---
+++ Model/Subscription.php
@@ -0,0 +1,61 @@
+<?php
+namespace JYSK\NewsletterEndpoint\Model;
+use JYSK\NewsletterEndpoint\Api\SubscriptionInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+class Subscription implements SubscriptionInterface
+{
+    protected $subscriberFactory;
+
+    public function __construct(
+        \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory,
+        \Magento\Customer\Model\CustomerFactory $customer,
+        \Magento\Store\Model\StoreManagerInterface $storemanager)
+    {
+        $this->subscriberFactory = $subscriberFactory;
+        $this->customer = $customer;
+        $this->storemanager = $storemanager;
+    }
+
+    /**
+     * Returns user email
+     * @param string $email
+     * @return void
+     */
+    public function subscribeByEmail($email)
+    {
+        $websiteId = $this->storemanager->getStore()->getWebsiteId();
+        $customerId = $this->customer->create()->setWebsiteId($websiteId)->loadByEmail($email)->getId();
+        if ($customerId) {
+            $this->subscriberFactory->create()->subscribeCustomerById($customerId);
+        } else {
+            $this->subscriberFactory->create()->subscribe($email);
+        }
+    }
+
+    /**
+     * @param string $email
+     * @return void
+     */
+    public function isSubscribed($email)
+    {
+        if (!$this->subscriberFactory->create()->loadByEmail($email)->isSubscribed()) {
+            throw new NoSuchEntityException(__('Email not subscribed'));
+        }
+    }
+
+    /**
+     * @param string $email
+     * @return void
+     */
+    public function deleteByEmail($email)
+    {
+        $websiteId = $this->storemanager->getStore()->getWebsiteId();
+        $customerId = $this->customer->create()->setWebsiteId($websiteId)->loadByEmail($email)->getId();
+        if ($customerId) {
+            $this->subscriberFactory->create()->unsubscribeCustomerById($customerId);
+        } else {
+            $this->subscriberFactory->create()->loadByEmail($email)->unsubscribe();
+        }
+    }
+}
